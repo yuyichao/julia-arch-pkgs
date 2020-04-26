67c99eb247b409fbb9b2b3daf8462655d48dd3a7
Yichao Yu <yyc1992@gmail.com>
2020-04-26 11:00:51 -0400

    [scripts] return package info in from repo

--- scripts/ArchPkgTools/src/repo.jl
+++ scripts/ArchPkgTools/src/repo.jl
@@ -8,11 +8,12 @@ using Pkg.TOML
 
 function get_all_pkgs(repo_dir)
     pkg_dir = joinpath(repo_dir, "pkgs")
-    pkgs = Set{String}()
+    pkgs = Dict{String,Any}()
     for d in readdir(pkg_dir)
         pkg_path = joinpath(pkg_dir, d, "pkg.toml")
         isfile(pkg_path) || continue
-        push!(pkgs, TOML.parsefile(pkg_path)["name"])
+        info = TOML.parsefile(pkg_path)
+        pkgs[info["name"]] = info
     end
     return pkgs
 end
